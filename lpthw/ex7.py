24c78dbd344f281d3e05ff59e609575aa28890b3
Jayam Arun <83112248+jayamarunr@users.noreply.github.com>
2023-09-07 20:17:04 +0530

    Update ex7

--- lpthw/ex7.py
+++ lpthw/ex7.py
@@ -1,7 +1,7 @@
 print("Mary had a little lamb.")
 print("Its fleece was white as {}.".format('snow'))
-print("And everwhere that Mary went.")
-print("."*10) #what'd that do?
+print("And everywhere that Mary went.")
+print("."*10) # what'd that do?
 
 end1 = "C"
 end2 = "h"
@@ -16,6 +16,6 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-# watch end = ' ' at the end. try removing it to see what happens
+#watch end = ' ' at the end. try removing it to see what happens
 print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
 print(end7 + end8 + end9 + end10 + end11 + end12)
