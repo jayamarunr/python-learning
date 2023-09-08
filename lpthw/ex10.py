f91808dffefd3ab4b1b68fc4ec0ce913d001e18e
Jayam Arun <83112248+jayamarunr@users.noreply.github.com>
2023-09-08 17:32:22 +0530

    Python Practice

---
+++ lpthw/ex10.py
@@ -0,0 +1,15 @@
+tabby_Cat = "\tI'm tabbed in."
+persian_Cat = "I'm split\non a line."
+backshlash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_Cat)
+print(persian_Cat)
+print(backshlash_cat)
+print(fat_cat)
