3da5419ec0be3c1f94f89ce703d85e63388dbaff
Jayam Arun <83112248+jayamarunr@users.noreply.github.com>
2023-09-09 13:18:11 +0530

    python practice

--- lpthw/ex13.py
+++ lpthw/ex13.py
@@ -2,7 +2,7 @@ from sys import argv
 #read the WYSS section for how to run this
 script, first, second, third = argv
 
-print("The script is called:", script)
-print("Your first variable is:", first)
-print("Your secon variable is:", second)
-print("Your third variable is:", third)
+print("Your script is", script)
+print("First argument is", first)
+print("Second argument is", second)
+print("Third argument is", third)
